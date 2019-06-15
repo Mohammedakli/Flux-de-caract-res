50da52158f291ad133c2f9a0011eccffce95d10b
Mohammed AKLI <mohammedakli05@gmail.com>
2019-06-15 17:32:13 +0200

    Create 10 files

--- FluxCarac/src/Flux.java
+++ FluxCarac/src/Flux.java
@@ -11,7 +11,7 @@ public class Flux {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		FileInputStream input = new FileInputStream("input.txt");
+		/*FileInputStream input = new FileInputStream("input.txt");
 		FileOutputStream output = new FileOutputStream("output.txt", true);
 		int c;
 		while((c = input.read()) != -1) {
@@ -20,7 +20,22 @@ public class Flux {
 		Path example = Paths.get("input.txt").toAbsolutePath(); 
 		System.out.println(example.getParent());
 		input.close();
-		output.close();
+		output.close();*/
+		
+		System.out.println(args[0]);
+		
+		if(args[0].equals("10")){
+			for(int i = 0; i < Integer.parseInt(args[0]); i++) {
+				//File f = File.createTempFile(i+"test",".txt");
+				File f = new File(i+".txt");
+				f.createNewFile();
+				System.out.println(f.getAbsolutePath());
+			}
+		}
+		
+		
+		
 	}
+	
 
 }
